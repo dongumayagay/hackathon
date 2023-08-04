f0df2652bf09fa84ea3150f4aa8f5e7d12edc146
Don Gumayagay <dngumayagay@gmail.com>
2023-08-04 09:31:15 +0800

    fix

--- src/routes/(app)/game/EndTurn.svelte
+++ src/routes/(app)/game/EndTurn.svelte
@@ -8,7 +8,7 @@
 <form action="?/end_turn" method="post">
 	<input type="hidden" name="game_id" value={$page.data.game_id} />
 	<input type="hidden" name="uid" value={opponent_uid} />
-	{#if game.turn}
+	{#if game?.turn}
 		<button class="btn btn-warning" disabled={game.turn !== $page.data.user.uid}>
 			{game.turn === $page.data.user.uid ? 'End Turn' : 'Wait'}
 		</button>
