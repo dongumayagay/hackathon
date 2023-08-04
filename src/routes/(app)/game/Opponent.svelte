84d2fa69c5daec6226f03c18f2a537801dc10e46
Don Gumayagay <dngumayagay@gmail.com>
2023-08-04 17:58:29 +0800

    tweened stats value

--- src/routes/(app)/game/Opponent.svelte
+++ src/routes/(app)/game/Opponent.svelte
@@ -4,16 +4,19 @@
 	import { doc, onSnapshot } from 'firebase/firestore';
 	import { onMount } from 'svelte';
 	import ShowLogs from './ShowLogs.svelte';
+	import { tweened } from 'svelte/motion';
 
 	export let opponent_uid = '';
 	/** @type {any}	 */
 	let player;
-
+	const hp = tweened(0);
+	const mp = tweened(0);
 	onMount(() => {
-		const unsub = onSnapshot(
-			doc(db, `players/${opponent_uid}`),
-			(snapshot) => (player = snapshot.data())
-		);
+		const unsub = onSnapshot(doc(db, `players/${opponent_uid}`), (snapshot) => {
+			player = snapshot.data();
+			$hp = player?.hp ?? 0;
+			$mp = player?.mp ?? 0;
+		});
 		return () => unsub();
 	});
 </script>
@@ -24,8 +27,8 @@
 			<img class="avatar rounded h-16" src={player?.photoURL ?? ''} alt="" />
 			<ul class="text-sm flex flex-col justify-between text-right">
 				<li>{player?.displayName ?? ''}</li>
-				<li>Security: {player?.hp ?? ''}</li>
-				<li>CPU Power: {player?.mp ?? ''}</li>
+				<li>Security: {$hp.toFixed(0) ?? ''}</li>
+				<li>CPU Power: {$mp.toFixed(0) ?? ''}</li>
 			</ul>
 		</div>
 	</div>
