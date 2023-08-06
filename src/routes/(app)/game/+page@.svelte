5da46a8f9f0dcd60c67b939c5bc6026a457f2acd
Don Gumayagay <dngumayagay@gmail.com>
2023-08-06 11:28:01 +0800

    simplify ui conditionals

--- src/routes/(app)/game/+page@.svelte
+++ src/routes/(app)/game/+page@.svelte
@@ -1,6 +1,5 @@
 <script>
 	// @ts-nocheck
-	import { playerStore } from '$lib/gamestate.js';
 
 	import { gameIdStore, userIdStore, gameStore } from '$lib/gamestate.js';
 	import ShareGame from '$lib/components/ShareGame.svelte';
@@ -8,56 +7,51 @@
 	import { browser } from '$app/environment';
 	import Opponent from '$lib/components/Opponent.svelte';
 	import Player from '$lib/components/Player.svelte';
-	import { onMount } from 'svelte';
 
 	export let data;
 
-	// $: if (browser) {
-	// 	$gameIdStore = data.game_id;
-	// 	$userIdStore = data.user.uid;
-	// }
-	onMount(() => {
+	$: if (browser) {
 		$gameIdStore = data.game_id;
 		$userIdStore = data.user.uid;
-	});
+	}
 </script>
 
-<div
-	style="height:100dvh; background-image: url('/Background.jpg');  display: grid;
+{#if $gameStore === null}
+	<div
+		class="hero min-h-screen bg-base-200 text-white relative"
+		style="background-image: url('/bg.jpg');"
+	>
+		<div class="hero-overlay bg-zinc-900/70" />
+		<div class="hero-content text-center flex flex-col">
+			<h1 class="text-3xl font-bold text-center">Game Loading...</h1>
+		</div>
+	</div>
+{:else if $gameStore?.phase === 'wait'}
+	<div
+		class="hero min-h-screen bg-base-200 text-white relative"
+		style="background-image: url('/bg.jpg');"
+	>
+		<div class="hero-overlay bg-zinc-900/70" />
+		<div class="hero-content text-center flex flex-col">
+			<h1 class="text-3xl font-bold text-center">Waiting for Opponent...</h1>
+			<div class="max-w-md flex flex-col gap-4">
+				<ShareGame />
+				<CancelLobby />
+			</div>
+		</div>
+	</div>
+{:else if $gameStore?.phase === 'start'}
+	<div
+		style="height:100dvh; background-image: url('/Background.jpg');  display: grid;
     width: 100%;
     background-size: cover;
     background-position: center;"
-	class=" flex flex-col min-h-full"
->
-	{#if $gameStore?.phase === 'start'}
+		class=" flex flex-col min-h-full"
+	>
 		<main class="flex-1 flex flex-col justify-between p-4">
 			<Opponent />
 
 			<Player />
 		</main>
-	{:else if $gameStore?.phase === 'wait'}
-		<div
-			class="hero min-h-screen bg-base-200 text-white relative"
-			style="background-image: url('/bg.jpg');"
-		>
-			<div class="hero-overlay bg-zinc-900/70" />
-			<div class="hero-content text-center flex flex-col">
-				<h1 class="text-3xl font-bold text-center">Waiting for Opponent...</h1>
-				<div class="max-w-md flex flex-col gap-4">
-					<ShareGame />
-					<CancelLobby />
-				</div>
-			</div>
-		</div>
-	{:else}
-		<div
-			class="hero min-h-screen bg-base-200 text-white relative"
-			style="background-image: url('/bg.jpg');"
-		>
-			<div class="hero-overlay bg-zinc-900/70" />
-			<div class="hero-content text-center flex flex-col">
-				<h1 class="text-3xl font-bold text-center">Game Loading...</h1>
-			</div>
-		</div>
-	{/if}
-</div>
+	</div>
+{/if}
